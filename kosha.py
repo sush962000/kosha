99c6c38d62fce01bf7fb808517b2cbde281b5377
Susmita Ghose <g.susmita@gmail.com>
2015-01-28 23:35:22 -0800

    Hookup new recommender engine.

--- kosha.py
+++ kosha.py
@@ -1,56 +1,38 @@
-from flask import Flask, json, jsonify, make_response, request, session
-import recommender
+from flask import Flask, jsonify, make_response, request, session
+from recommender import Recommender
 from uuid import uuid4
 
 app = Flask(__name__)
 app.debug = True
 app.secret_key = 'development key'
 
-data = recommender.load("restaurant_data.csv")
-
-class BusinessData:
-  def __init__(self):
-    with open('restaurant_data.json') as f:
-      self.restaurants = [json.loads(line) for line in f.readlines()]
-  def filter(self, name, max_count=30):
-    restaurants = self.restaurants
-    if name:
-      lower_name = name.lower()
-      restaurants = [r for r in restaurants if lower_name in r['name'].lower()]
-    if max_count and len(restaurants) > max_count:
-      restaurants = restaurants[0 : max_count]
-    return restaurants
-business_data = BusinessData()
+recommender = Recommender(
+    observation_data_filepath="observation_data.csv",
+    item_data_filepath="item_data.csv")
 
 @app.route("/")
 def main():
+  if not "user_id" in session:
+    user_id = str(uuid4())
+    session["user_id"] = user_id
   return app.send_static_file("index.html")
 
 @app.route("/restaurants", methods=["GET"])
 def api_restaurants():
-  restaurant_name = request.args.get('name')
-  if restaurant_name:
-    recommendations = business_data.filter(restaurant_name)
-  elif not "id" in session:
-    # New user
-    session["id"] = str(uuid4())
-    recommendations = recommender.recommend_by_popularity(data)
-  else:
-    # Existing user.
-    recommendations = recommender.recommend_for_user(data, session["id"])
-
-  response = jsonify(items=recommendations)
+  top_items = recommender.recommend(
+      user_id=session["user_id"],
+      name_filter=request.args.get('name'))
+  response = jsonify(items=top_items)
   response.status_code = 200
   return response
 
-@app.route("/restaurants/<id>", methods=["PATCH"])
-def api_post_rating(id):
-  global data
+@app.route("/restaurants/<item_id>", methods=["PATCH"])
+def api_post_rating(item_id):
   rating = request.json["rating"]
-  print id
-  print rating
-  data = recommender.add_rating(data, id, session["id"], rating)
-  print len(data)
+  recommender.add_rating(
+      user_id=session["user_id"],
+      item_id=item_id,
+      rating=rating)
   response = make_response()
   response.status_code = 204
   return response
--- recommender.py
+++ recommender.py
@@ -1,23 +1,29 @@
 import graphlab as gl
-import pandas as pd
+
+def _has_user(observation_data, user_id):
+  # Returns True if the observation_data has the given user_id.
+  if not observation_data:
+    return False
+  return user_id in observation_data['user_id']
 
 class Recommender:
   def __init__(self, observation_data_filepath, item_data_filepath):
-    self.observation_data = gl.SFrame.read_csv(observation_data_filepath)
+    self.observation_data = gl.SFrame.read_csv(
+        observation_data_filepath,
+        column_type_hints={"rating":int})
     self.item_data = gl.SFrame.read_csv(item_data_filepath)
-    self.new_observation_data = gl.SFrame({'user_id': [], 'item_id': [], 'rating': []})
-
+    self.new_observation_data = None
     self.popularity_recommender = gl.popularity_recommender.create(
-        observation_data,
+        self.observation_data,
         target='rating',
-        verbose=True)
+        verbose=False)
     self.ranking_factorization_recommender = gl.ranking_factorization_recommender.create(
-        observation_data,
+        self.observation_data,
         target='rating',
         num_factors=25,
         regularization=0.01,
         max_iterations=10,
-        verbose=True)
+        verbose=False)
 
   def add_rating(self, user_id, item_id, rating):
     row = gl.SFrame({
@@ -25,40 +31,39 @@ class Recommender:
         'item_id': [item_id],
         'rating': [rating]
     })
-    self.new_observation_data.append(row)
+    self.new_observation_data = \
+        self.new_observation_data.append(row) if self.new_observation_data else row
 
   def recommend(self, user_id, max_count=30, name_filter=None):
-    recommender = self.ranking_factorization_recommender if self.__is_existing_user(user_id) else self.popularity_recommender
+    recommender = self.ranking_factorization_recommender \
+        if self.__is_existing_user(user_id) else self.popularity_recommender
     top_items = recommender.recommend(
         users=[user_id],
         k=max_count,
-        items=__filter_items() if name_filter else None,
-        new_observation_data=new_observation_data,
-        verbose=True)
-    return __to_json(top_items)
+        items=self.__filter_items(name_filter) if name_filter else None,
+        new_observation_data=self.new_observation_data,
+        verbose=False)
+    return self.__to_json(top_items)
 
   def __is_existing_user(self, user_id):
     # Returns True if the given user has rated at least one restaurant.
-    return __has_user(self.new_observation_data) #or __has_user(self.observation_data)
-
-  def __has_user(observation_data, user_id):
-    # Returns True if the observation_data has the given user_id.
-    return observation_data[observation_data['user_id'] == user_id]['user_id'].size() != 0
+    return _has_user(self.new_observation_data, user_id) #or has_user(self.observation_data)
 
   def __filter_items(self, name_filter):
     # Returns an item_id SArray that satisfies the given filter.
-    lower_name = name_filter.lower()
-    filtered_sf = self.item_data[lower_name in self.item_data['name'].lower()]
-    return filtered_sf['item_id']
+    item_ids = []
+    lower_name = name_filter.lower().encode('utf-8')
+    for index, name in enumerate(self.item_data['name']):
+      if lower_name in name.lower():
+        item_ids.append(self.item_data['item_id'][index])
+    return item_ids
 
-  def __to_json(self, recommendation_sf):
-    recommendation_df = recommendation_sf.to_dataframe()
+  def __to_json(self, top_items):
     recommendations = []
-    for row in recommendation_df.iterrows():
-        item_id = row['item_id']
-        name = self.item_data[self.item_data['item_id'] == item_id]['name'][0]
-        rating = row['score']
-        recommendations.append({'id':item_id, 'name':name, 'rating': rating})
+    for index, item_id in enumerate(top_items['item_id']):
+      name = self.item_data[self.item_data['item_id'] == item_id]['name'][0]
+      rating = top_items['score'][index]
+      recommendations.append({'id':item_id, 'name':name, 'rating': rating})
     return recommendations
 
 def train_restaurant_data():
@@ -73,8 +78,8 @@ def train_restaurant_data():
     #train_set_1, test_set = gl.recommender.util.random_split_by_user(
                                     #high_rated_data, user_id='user', item_id='restaurant')
     #train_set = train_set_1.append(low_rated_data)
-    (train_set, test_set) = gl.recommender.util.random_split_by_user(data, user_id='user', item_id='restaurant',
-        max_num_users=1000, item_test_proportion=0.2)
+    (train_set, test_set) = gl.recommender.util.random_split_by_user(
+        data, user_id='user', item_id='restaurant', max_num_users=1000, item_test_proportion=0.2)
     #(train_set, test_set) = data.random_split(0.8)
     m1 = gl.popularity_recommender.create(train_set, 'user', 'restaurant_id', 'rating')
     baseline_rmse = gl.evaluation.rmse(test_set['rating'], m1.predict(test_set))
@@ -82,8 +87,9 @@ def train_restaurant_data():
 
     regularization_vals = [0.001, 0.01, 0.05, 0.1]
     number_of_factors = [2, 5, 10, 25]
-    models = [gl.factorization_recommender.create(train_set, 'user', 'restaurant_id', 'rating',
-                                              max_iterations=50, num_factors= 25, regularization= 0.01, verbose = False)]
+    models = [gl.factorization_recommender.create(
+        train_set, 'user', 'restaurant_id', 'rating', max_iterations=50, num_factors= 25,
+        regularization= 0.01, verbose = False)]
 
     # Save the train and test RMSE, for each model
     (rmse_train, rmse_test) = ([], [])
@@ -94,46 +100,9 @@ def train_restaurant_data():
     print rmse_train
     print rmse_test
 
-def load(filepath):
-    return gl.SFrame.read_csv(filepath, column_type_hints={"rating":int})
-
-def add_rating(data, id, user, rating):
-    name = data[data["id"] == id]["name"][0]
-    new_rating = gl.SFrame({"id": [id], "name" : [name], "user": [user], "rating":[rating], "X1":["X1"]})
-    return data.append(new_rating)
-
-def get_rating(data, id, user):
-    return 3
-
-def recommend_for_user(data, user, max_count=30):
-    mf_model = gl.ranking_factorization_recommender.create(
-        data, 'user', 'id', 'rating',
-        max_iterations=10, num_factors=25, regularization=0.01, verbose=True)
-    model_recommendations = mf_model.recommend(users=[user], k=max_count)
-    recommendations_data = model_recommendations.to_dataframe()
-    recommendations = []
-    for index, rows in recommendations_data.iterrows():
-        id = rows["id"]
-        name = data[data["id"] == id]["name"][0]
-        recommendations.append({"id":id, "name":name, "rating" : rows["score"]})
-    return recommendations
-
-def recommend_by_popularity(data, max_count=30):
-    m = gl.popularity_recommender.create(data, 'user', 'id', 'rating', verbose=False)
-    model_recommendations = m.recommend(k=5)
-
-    recommendations_data = model_recommendations.to_dataframe()
-    recommendations = []
-    for index, rows in recommendations_data.iterrows():
-        if index >= max_count: break
-        id = rows["id"]
-        name = data[data["id"] == id]["name"][0]
-        recommendations.append({"id":id, "name":name, "rating" : rows["score"]})
-    return recommendations
-
 def main():
-    data = load("restaurant_data.csv")
-    recommendations = recommend_by_popularity(data)
+    engine = Recommender("observation_data.csv", "item_data.csv")
+    recommendations = engine.recommend('foo')
     print recommendations
 
 if __name__ == '__main__':
